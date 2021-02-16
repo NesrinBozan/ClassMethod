bec97f72ba8a8ed646aaa65786d41f611c544521
nesrinnbozan6@gmail.com <nesrinnbozan6@gmail.com>
2021-02-16 18:48:44 +0300

    16.02.2021

---
+++ ClassMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Product[] products = new Product[] { urun1, urun2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("----------");
+            }
+
+            Console.WriteLine("--------METOTLAR--------");
+            //encapsulation
+            SepetMenager sepetMenager = new SepetMenager();
+            sepetMenager.Add(urun1);
+            sepetMenager.Add(urun2);
+            sepetMenager.Add2("Armut", "Yeşil armmut", 12,85);
+
+            sepetMenager.Add2("Elma ","Yeşil elma", 13, 55);
+
+        }
+    }
+}
+
+// Dont repeat yourself DRY, Clean Code , Best Practice
---
+++ ClassMethod/SepetMenager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethod
+{
+    class SepetMenager
+    {
+        //naming convertion
+        public void Add(Product product)
+        {
+
+            Console.WriteLine("Tebrikler. Sepete eklendi:" + product.Adi);
+
+        }
+        public void Add2(string urunAdi,string aciklama,double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi:" + urunAdi);
+        }
+    }
+}
