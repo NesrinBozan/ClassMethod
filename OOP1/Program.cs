81ae9de90ded22a682296f70720c8dff648a641d
nesrinnbozan6@gmail.com <nesrinnbozan6@gmail.com>
2021-02-16 18:59:45 +0300

    16.02.2021

---
+++ OOP1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product1 product = new Product1();
+            product.Id = 1;
+            product.CategoryId = 2;
+            product.ProductName = "Masa";
+            product.UnitPrice = 500;
+            product.UnitPrice = 3;
+
+            Product1 product1 = new Product1 { Id = 2, CategoryId = 5, UnitsInStock = 5, UnitPrice=35,ProductName="Kalem" };
+            
+            //PascalCase     //camelCase
+            ProductManager productManager = new ProductManager();
+
+            
+        }
+    }
+}
